d6060e1bf270fb1f3cfecb12b9bdd5c60ed41dd5
kanekotic <me@kanekotic.com>
2019-11-30 20:02:47 -0600

    updated dependencies and rollup script

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,9 @@
-import { uglify } from "rollup-plugin-uglify"
+import { terser } from "rollup-plugin-terser"
 import commonjs from 'rollup-plugin-commonjs'
 import nodeResolve from 'rollup-plugin-node-resolve'
-import typescript from 'rollup-plugin-typescript'
-import replace from 'rollup-plugin-replace'
-import alias from 'rollup-plugin-alias'
-import path from 'path'
+import typescript from 'rollup-plugin-typescript2'
+import json from "rollup-plugin-json"
+import builtins from "rollup-plugin-node-builtins"
 
 export default {
     output: {
@@ -14,20 +13,15 @@ export default {
     },
     input: "lib/index.ts",
     plugins: [
-        typescript({
-            tsconfig: false,
-            target: "es5",
-            declaration: true,
-            strict: true,
-            lib: ["es6"],
-
-        }),
-        nodeResolve(), 
+        nodeResolve({ jsnext: true, preferBuiltins: true, browser: true }), 
         commonjs(), 
-        uglify(),
-        alias({
-            vue: path.resolve('./node_modules/vue/dist/vue.runtime.common.js')
+        builtins(),
+        typescript({
+            rollupCommonJSResolveHack: true,
+            objectHashIgnoreUnknownHack: true,
+            tsconfigOverride: { compilerOptions: { module: "ESNext" } }
         }),
-        replace({'process.env.NODE_ENV': JSON.stringify('development')})
+        terser(),
+        json()    
     ]
 }
